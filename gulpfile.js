9dde801041cce5353ab472c03cb947cb56fea662
Wayne <dev.wayne.ix@gmail.com>
2015-02-04 17:11:39 +0800

    init&line

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+
+gulp.task('img', function(){
+  gulp.src('src/img/**/*')
+    .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('less', function() {
+  gulp.src('src/index.less')
+    .pipe(less({
+      paths: [
+        path.join(__dirname, 'less')
+      ]
+    }))
+    .on('error', console.error)
+    .pipe(gulp.dest('dist/'));
+
+  gulp.src('src/img/**/*')
+    .pipe(gulp.dest('dist/img/'));
+
+});
+
+gulp.task('default', ['less'])
